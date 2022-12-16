0c4328753d790b4a7da6ac03beaec7f075d4fc69
rin-gil <91410075+rin-gil@users.noreply.github.com>
2022-12-16 07:36:22 +0200

    refactor: Change logging imports
    Signed-off-by: rin-gil <91410075+rin-gil@users.noreply.github.com>

--- tgbot/misc/logger.py
+++ tgbot/misc/logger.py
@@ -1,13 +1,13 @@
 """Enables logging"""
 
-from logging import basicConfig, getLogger, INFO, Logger
+import logging
 
 from tgbot.config import LOG_FILE
 
 
-logger: Logger = getLogger(__name__)
-basicConfig(
+logger: logging.Logger = logging.getLogger(__name__)
+logging.basicConfig(
     filename=LOG_FILE,
-    level=INFO,
+    level=logging.INFO,
     format="%(levelname)-8s %(filename)s:%(lineno)d [%(asctime)s] - %(name)s - %(message)s",
 )
