7482fd2b276fc75d9e74510a8b5fe83a2d075126
rin-gil <91410075+rin-gil@users.noreply.github.com>
2023-04-24 14:36:40 +0300

    feat: added decorator to run the synchronous method asynchronously
    Signed-off-by: rin-gil <91410075+rin-gil@users.noreply.github.com>

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -5,7 +5,6 @@ from os import remove as os_remove
 from aiogram import Dispatcher
 from aiogram.types import InputFile, Message
 
-from tgbot.config import BOT_LOGO
 from tgbot.middlewares.localization import i18n
 from tgbot.services.database import database
 from tgbot.services.statistics import bot_statistics
@@ -16,19 +15,14 @@ _ = i18n.gettext  # Alias for gettext method
 async def if_admin_sent_command_stats(message: Message) -> None:
     """Shows statistics for administrators"""
     lang_code: str = message.from_user.language_code
-    path_to_statistics_graph: str | None = await bot_statistics.get_path_to_statistics_graph(
-        downloads_data=await database.get_statistics_data(), locale=lang_code
+    path_to_statistics_graph: str = await bot_statistics.get_path_to_statistics_graph(
+        await database.get_statistics_data(), lang_code
     )
-    if path_to_statistics_graph:
-        await message.reply_photo(
-            photo=InputFile(path_to_statistics_graph),
-            caption="📊 " + _("Bot statistics by months", locale=lang_code),
-        )
-        os_remove(path_to_statistics_graph)
-    else:
-        await message.reply_photo(
-            photo=InputFile(BOT_LOGO), caption="❌ " + _("Error in plotting the graph", locale=lang_code)
-        )
+    await message.reply_photo(
+        photo=InputFile(path_to_statistics_graph),
+        caption="📊 " + _("Bot statistics by months", locale=lang_code),
+    )
+    os_remove(path_to_statistics_graph)
 
 
 def register_admin(dp: Dispatcher) -> None:
--- tgbot/services/statistics.py
+++ tgbot/services/statistics.py
@@ -1,9 +1,8 @@
 """Functions for graphing downloads in the bot and saving the graph image"""
 
-from asyncio import get_running_loop
 from os import makedirs
 from os.path import exists, join
-from typing import NamedTuple
+from typing import Any, NamedTuple
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,7 +13,7 @@ from numpy import ndarray
 
 from tgbot.config import STATS_BG_IMAGE, TEMP_DIR
 from tgbot.middlewares.localization import i18n
-from tgbot.misc.logger import logger
+from tgbot.services.decorators import run_in_asyncio_thread
 
 _ = i18n.gettext  # Alias for gettext method
 
@@ -54,62 +53,61 @@ class PlottingBotStatistics:
         }
         return f"{month_in_user_local_language.get(date[5:])}\n{date[:4]}"
 
-    def _plot_download_graph(self, downloads_data: BotStatisticsData, locale: str) -> str | None:
-        """Builds an image of the download graph and saves it to a file"""
-        try:
-            figure: Figure = plt.figure(figsize=(12, 9))
-            axes: Axes = figure.add_subplot()
+    @run_in_asyncio_thread
+    def get_path_to_statistics_graph(self, bot_statistics_data: BotStatisticsData, locale: str) -> Any:
+        """
+        Plots an image of the bot's statistics graph and saves it to a file
 
-            # Let's get the data for the graph
-            dates: list[str] = [self._format_date(date=date, locale=locale) for date in downloads_data.date]
-            counters: dict[str, list[int]] = {
-                _("Downloads", locale=locale): downloads_data.downloads_counter,
-                _("Searches", locale=locale): downloads_data.searches_counter,
-            }
+        Note: Do not use named arguments when calling this method
 
-            # Making a chart
-            range_of_dates: ndarray = np.arange(len(dates))  # Number of values on the abscissa axis
-            width: float = 0.4  # The width of the bars
-            multiplier: float = 0.5  # Offset of bars relative to the center of the container
+        Args:
+            bot_statistics_data (): Data on bot usage statistics
+            locale (): Telegram user language
 
-            for key, value in counters.items():
-                offset: float = width * multiplier
-                rects: BarContainer = axes.bar(x=range_of_dates + offset, height=value, width=width, label=key)
-                axes.bar_label(container=rects, padding=3)
-                multiplier += 1
+        Returns:
+            Path to the saved image file with the graph or None
+        """
 
-            axes.invert_xaxis()
+        figure: Figure = plt.figure(figsize=(12, 9))
+        axes: Axes = figure.add_subplot()
 
-            # Add explanatory labels
-            axes.set_title(label=_("Chart of bot usage by date", locale=locale))
-            axes.set_yticks([])  # Remove labels on the ordinate axis
-            axes.set_xticks(range_of_dates + width, dates)  # Add the labels on the abscissa axis
-            axes.legend(loc="upper left", ncols=2)  # Legend for charts
+        # Let's get the data for the graph
+        dates: list[str] = [self._format_date(date=date, locale=locale) for date in bot_statistics_data.date]
+        counters: dict[str, list[int]] = {
+            _("Downloads", locale=locale): bot_statistics_data.downloads_counter,
+            _("Searches", locale=locale): bot_statistics_data.searches_counter,
+        }
 
-            # Add background image on chart
-            bg_image: ndarray = plt.imread(fname=STATS_BG_IMAGE, format="png")
-            x_min, x_max = axes.get_xlim()
-            y_min, y_max = axes.get_ylim()
-            axes.imshow(bg_image, extent=(x_min, x_max, y_min, y_max), aspect="auto", alpha=0.3)
+        # Making a chart
+        range_of_dates: ndarray = np.arange(len(dates))  # Number of values on the abscissa axis
+        width: float = 0.4  # The width of the bars
+        multiplier: float = 0.5  # Offset of bars relative to the center of the container
 
-            # Save the chart to a file
-            path_to_statistics_graph: str = join(TEMP_DIR, "stats.png")
-            figure.savefig(fname=path_to_statistics_graph)
+        for key, value in counters.items():
+            offset: float = width * multiplier
+            rects: BarContainer = axes.bar(x=range_of_dates + offset, height=value, width=width, label=key)
+            axes.bar_label(container=rects, padding=3)
+            multiplier += 1
 
-            return path_to_statistics_graph
+        axes.invert_xaxis()
 
-        except Exception as ex:
-            logger.info("Error in plotting the graph: %s", ex)
+        # Add explanatory labels
+        axes.set_title(label=_("Chart of bot usage by date", locale=locale))
+        axes.set_yticks([])  # Remove labels on the ordinate axis
+        axes.set_xticks(range_of_dates + width, dates)  # Add the labels on the abscissa axis
+        axes.legend(loc="upper left", ncols=2)  # Legend for charts
 
-        return None
+        # Add background image on chart
+        bg_image: ndarray = plt.imread(fname=STATS_BG_IMAGE, format="png")
+        x_min, x_max = axes.get_xlim()
+        y_min, y_max = axes.get_ylim()
+        axes.imshow(bg_image, extent=(x_min, x_max, y_min, y_max), aspect="auto", alpha=0.3)
 
-    async def get_path_to_statistics_graph(self, downloads_data: BotStatisticsData, locale: str) -> str | None:
-        """Returns the path to the graph image"""
+        # Save the chart to a file
+        path_to_statistics_graph: str = join(TEMP_DIR, "stats.png")
+        figure.savefig(fname=path_to_statistics_graph)
 
-        path_to_graph_image: str | None = await get_running_loop().run_in_executor(
-            None, self._plot_download_graph, downloads_data, locale
-        )
-        return path_to_graph_image
+        return path_to_statistics_graph
 
 
 bot_statistics: PlottingBotStatistics = PlottingBotStatistics()
