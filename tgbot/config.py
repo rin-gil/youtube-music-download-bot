88446d31783fc03970123e86926edbe878ac7770
rin-gil <91410075+rin-gil@users.noreply.github.com>
2023-07-07 08:13:58 +0300

    refactor: update logging
    Signed-off-by: rin-gil <91410075+rin-gil@users.noreply.github.com>

--- tgbot/config.py
+++ tgbot/config.py
@@ -11,7 +11,7 @@ _BASE_DIR: Path = Path(__file__).resolve().parent.parent
 DB_FILE: str = normpath(join(_BASE_DIR, "tgbot/db.sqlite3"))
 LOCALES_DIR: str = normpath(join(_BASE_DIR, "tgbot/locales"))
 TEMP_DIR: str = normpath(join(_BASE_DIR, "tgbot/temp"))
-LOG_FILE: str = join(_BASE_DIR, "log.log")
+LOG_FILE: str = join(_BASE_DIR, "youtube-music-download-bot.log")
 BOT_LOGO: str = normpath(join(_BASE_DIR, "tgbot/assets/img/bot_logo.jpg"))
 STATS_BG_IMAGE: str = normpath(join(_BASE_DIR, "tgbot/assets/img/stats_bg.png"))
 
