fce084927ba34b063107194e63773510be1c8cdb
rin-gil <91410075+rin-gil@users.noreply.github.com>
2023-08-04 19:14:59 +0300

    refactor: update translations
    Signed-off-by: rin-gil <91410075+rin-gil@users.noreply.github.com>

--- src/tgbot/middlewares/localization.py
+++ src/tgbot/middlewares/localization.py
@@ -5,4 +5,4 @@ from aiogram.contrib.middlewares.i18n import I18nMiddleware
 from tgbot.config import LOCALES_DIR
 
 
-i18n: I18nMiddleware = I18nMiddleware(domain="tgbot", path=LOCALES_DIR)
+i18n: I18nMiddleware = I18nMiddleware(domain="messages", path=LOCALES_DIR)
